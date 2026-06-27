cc2abb65f6f7cb0e3de185deb77ce626b7992b1b
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-06-27 16:35:35 +0530

    Time: 1 ms (99.95%), Space: 77.1 MB (81.73%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = 0 , maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            currentSum += nums[i];
+            maxSum = Math.max(maxSum , currentSum);
+            if(currentSum < 0) currentSum = 0;
+        }
+        return maxSum;
+    }
+}
