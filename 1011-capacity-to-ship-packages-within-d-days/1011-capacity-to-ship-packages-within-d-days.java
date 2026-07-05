d84a35a96ebb027f9548ee7bfb23a3131e82f9ca
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-07-05 15:02:22 +0530

    Time: 13 ms (14.44%), Space: 50.5 MB (6.4%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int max(int[] weights){
+        int MAX = Integer.MIN_VALUE;
+        for(int i=0;i<weights.length;i++){
+            MAX = Math.max(MAX , weights[i]);
+        }
+        return MAX;
+    }
+    public int SUM(int[] weights){
+        int sum = 0;
+        for(int i=0;i<weights.length;i++){
+            sum += weights[i];
+        }
+        return sum;
+    }
+    public boolean isFeasible(int mid , int[] weights , int days){
+        int Days = 1 , capacity = 0;
+        for(int i=0;i<weights.length;i++){
+            if(capacity + weights[i] > mid){
+                capacity = weights[i];
+                Days++;
+            }else{
+               capacity += weights[i];
+            }
+        }
+
+        if(Days <= days) return true;
+        return false;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int low = max(weights) , high = SUM(weights) , res = -1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            boolean ans = isFeasible(mid , weights , days);
+            if(ans){
+                res = mid;
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+}
