0198118b198f580b94ac8cd4fc591ec8dfcbc572
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-07-17 11:46:54 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,25 @@
 class Solution {
-    public int find_ans(int n , int[] dp){
-        if(n==0 || n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        dp[n] = find_ans(n-1 , dp) + find_ans(n-2 , dp);
-        return dp[n];
-    }
+    // public int find_ans(int n , int[] dp){
+    //     if(n==0 || n==1) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     dp[n] = find_ans(n-1 , dp) + find_ans(n-2 , dp);
+    //     return dp[n];
+    // }
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        for(int i=0;i<dp.length;i++){
-            dp[i] = -1;
+        // int[] dp = new int[n+1];
+        // for(int i=0;i<dp.length;i++){
+        //     dp[i] = -1;
+        // }
+        // return find_ans(n,dp);
+
+        // TABULATION
+
+        int prev1 = 1 , prev2 = 1 , curr = 0;
+        for(int i=1;i<n;i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return find_ans(n,dp);
+        return prev1;
     }
 }
