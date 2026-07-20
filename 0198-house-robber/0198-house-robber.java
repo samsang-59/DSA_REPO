e19fb6ca9ff6cad1dced18b4c61ac23b758eaa71
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-07-20 19:10:17 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -9,11 +9,27 @@ class Solution {
         dp[n] = Math.max(pick , not_pick); 
         return dp[n];
     }
+    public int tabulation(int[] dp , int[] nums){
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0] , nums[1]);
+
+        for(int i=2;i<nums.length;i++){
+            int pick = dp[i-2] + nums[i];
+            int not_pick = dp[i-1] + 0;
+            dp[i] = Math.max(pick , not_pick);
+        }
+        return dp[nums.length-1];
+    }
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
+        
         int[] dp = new int[nums.length];
         for(int i=0;i<dp.length;i++){
             dp[i] = -1;
         }
-        return find_ans(nums , nums.length-1 , dp);
+
+        // return find_ans(nums , nums.length-1 , dp);
+        
+        return tabulation(dp , nums);
     }
 }
