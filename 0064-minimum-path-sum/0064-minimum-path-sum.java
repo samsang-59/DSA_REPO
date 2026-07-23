3096a43a018f929dd40f035efd89667e7e31871f
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-07-23 11:34:47 +0530

    Time: 5 ms (25.65%), Space: 50.3 MB (56.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -33,6 +33,27 @@ class Solution {
         return dp[dp.length-1][dp[0].length-1];
     }
 
+    public int space_optimization(int[][] grid){
+
+        int[] prev = new int[grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            int[] curr = new int[grid[0].length];
+            curr[0] = prev[0] + grid[i][0];
+            for(int j=0;j<grid[0].length;j++){
+                if(i==0 && j==0) curr[j] = grid[i][j];
+                else if(i==0 && j > 0) curr[j] = curr[j-1] + grid[i][j];
+                else if(i > 0 && j==0) curr[j] = prev[j] + grid[i][j];
+                else{
+                    int downSum = prev[j] + grid[i][j];
+                    int rightSum = curr[j-1] + grid[i][j];
+                    curr[j] = Math.min(downSum , rightSum);
+                }
+            }
+            prev = curr;
+        }
+        return prev[grid[0].length-1];
+    }
+
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -50,8 +71,11 @@ class Solution {
         // return memoization(m-1 , n-1 , grid , dp);
 
         // TABULATION
-        int[][] dp = new int[m][n];
-        return tabulation(dp , grid);
+        // int[][] dp = new int[m][n];
+        // return tabulation(dp , grid);
+
+        // SPACE_OPTIMIZATION
+        return space_optimization(grid);
          
     }
 }
