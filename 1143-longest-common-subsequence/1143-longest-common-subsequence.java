78ca9ce43e2f5447d899a385a80aa6b4d89f60d3
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-07-31 19:19:57 +0530

    Time: 32 ms (9.41%), Space: 53.8 MB (82.31%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int recurrsion(String text1 , String text2 , int i , int j){
+        if(i==text1.length() || j==text2.length()) return 0;
+
+        int pick = 0 , not_pick = 0;
+        if(text1.charAt(i)==text2.charAt(j)){
+            pick = 1 + recurrsion(text1 , text2 , i+1 , j+1);
+        }
+        else{
+            not_pick = Math.max(recurrsion(text1 , text2 , i+1 , j),recurrsion(text1 , text2 , i , j+1));
+        }
+        return Math.max(pick , not_pick);
+    }
+
+    public int memoization(String text1 , String text2 , int i , int j , int[][] dp){
+        if(i==text1.length() || j==text2.length()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int pick = 0 , not_pick = 0;
+        if(text1.charAt(i)==text2.charAt(j)) {
+            pick = 1 + memoization(text1 , text2 , i+1 , j+1 , dp);
+        }
+        else{
+            not_pick = Math.max(memoization(text1 , text2 , i+1 , j , dp),memoization(text1 , text2 , i , j+1 , dp));
+        }
+        dp[i][j] = Math.max(pick , not_pick);
+        return dp[i][j];
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        
+        // RECURRSION
+        // return recurrsion(text1 , text2 , 0 , 0);
+
+        // MEMOIZATION
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+        return memoization(text1 , text2 , 0 , 0 , dp);
+    }
+}
