2ffcf0229039d44a4ebe162ff18948c4fe28b535
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-07-31 19:31:26 +0530

    Time: 22 ms (45.69%), Space: 54 MB (52.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -27,19 +27,48 @@ class Solution {
         return dp[i][j];
     }
 
+    public int tabulation(String text1 , String text2 , int[][] dp){
+        for(int i=0;i<dp[0].length;i++){
+            dp[text1.length()][i] = 0;
+        }
+
+        for(int j=0;j<dp.length;j++){
+            dp[j][text2.length()] = 0;
+        }
+
+        for(int i = text1.length()-1;i>=0;i--){
+            for(int j = text2.length()-1;j>=0;j--){
+                int pick = 0 , not_pick = 0;
+                if(text1.charAt(i)==text2.charAt(j)) {
+                    pick = 1 + dp[i+1][j+1];
+                }
+                else{
+                    not_pick = Math.max(dp[i+1][j] , dp[i][j+1]);
+                }
+
+                dp[i][j] = Math.max(pick , not_pick);
+            }
+        }
+        return dp[0][0];
+    }
+
     public int longestCommonSubsequence(String text1, String text2) {
         
         // RECURRSION
         // return recurrsion(text1 , text2 , 0 , 0);
 
         // MEMOIZATION
-        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        // int[][] dp = new int[text1.length()+1][text2.length()+1];
 
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return memoization(text1 , text2 , 0 , 0 , dp);
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return memoization(text1 , text2 , 0 , 0 , dp);
+
+        // TABULATION
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        return tabulation(text1 , text2 , dp);
     }
 }
