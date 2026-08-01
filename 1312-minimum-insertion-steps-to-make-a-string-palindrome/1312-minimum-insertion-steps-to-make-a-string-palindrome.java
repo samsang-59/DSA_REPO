078085fac481789222da026373b63209926f05a6
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-08-01 14:54:35 +0530

    Time: 25 ms (64.41%), Space: 46.4 MB (94.04%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -27,6 +27,56 @@ class Solution {
         return dp[i][j];
     }
 
+    public int tabulation(String s , String rev , int[][] dp){
+        for(int i=0;i<dp[0].length;i++){
+            dp[s.length()][i] = 0;
+        }
+
+        for(int j=0;j<dp.length;j++){
+            dp[j][rev.length()] = 0;
+        }
+
+        for(int i=s.length()-1;i>=0;i--){
+            for(int j=rev.length()-1;j>=0;j--){
+                int pick = 0 , not_pick = 0;
+                if(s.charAt(i)==rev.charAt(j)){
+                    pick = 1 + dp[i+1][j+1];
+                }
+                else{
+                    not_pick = Math.max(dp[i+1][j] , dp[i][j+1]);
+                }
+                dp[i][j] = Math.max(pick , not_pick);
+            }
+        }
+        return dp[0][0];
+    }
+
+    public int space_optimization(String s , String rev){
+        int[] next = new int[s.length()+1];
+
+        for(int i=0;i<next.length;i++){
+            next[i] = 0;
+        }
+
+        for(int i=s.length()-1;i>=0;i--){
+            int[] curr = new int[rev.length()+1];
+            curr[rev.length()] = 0;
+            for(int j=rev.length()-1;j>=0;j--){
+                int pick = 0 , not_pick = 0;
+                if(s.charAt(i)==rev.charAt(j)){
+                    pick = 1 + next[j+1];
+                }
+                else{
+                    not_pick = Math.max(next[j] , curr[j+1]);
+                }
+                curr[j] = Math.max(pick , not_pick);
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+
+
     public int minInsertions(String s) {
         
         String rev = new StringBuilder(s).reverse().toString();
@@ -34,13 +84,20 @@ class Solution {
         // return s.length() - recurrsion(s , rev , 0 , 0);
 
         // MEMOIZATION
-        int[][] dp = new int[s.length()+1][rev.length()+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp.length;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return s.length() - memoization(s , rev , 0 , 0 , dp);
+        // int[][] dp = new int[s.length()+1][rev.length()+1];
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp.length;j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return s.length() - memoization(s , rev , 0 , 0 , dp);
+
+        // TABULATION
+        // int[][] dp = new int[s.length()+1][rev.length()+1];
+        // return tabulation(s , rev , dp);
+
+        // SPACE_OPTIMIZATION
+        return s.length() - space_optimization(s , rev);
 
     }
 }
