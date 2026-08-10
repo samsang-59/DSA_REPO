8266f2a0e7c4421663ded6cffc754477c955588c
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-08-10 14:22:29 +0530

    Time: 18 ms (34.2%), Space: 46.3 MB (38.39%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,32 @@
 class Solution {
-    public int characterReplacement(String s, int k) {
-        int start = 0 , end = 0 , maxLen = 0 , maxFreq = 0;
-        int[] counts = new int[26];
-
-        while(end < s.length()){
-           counts[s.charAt(end) - 'A']++;
-
-           maxFreq = Math.max(maxFreq , counts[s.charAt(end) - 'A']);
-
-            if((end - start + 1) - maxFreq  > k){
-                counts[s.charAt(start) - 'A']--;
-                start++;
-            }
-
-            if((end - start + 1) - maxFreq  <= k){
-                maxLen = Math.max(maxLen , end - start + 1);
-            }
-            end++;
+    public int max_frequency(int[] hash){
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<hash.length;i++){
+            max = Math.max(max , hash[i]);
         }
-        return maxLen;
+        return max;
+    }
+    public int characterReplacement(String s, int k) {
+       
+       int[] hash = new int[26];
+
+       int low = 0 , high = 0 , maxLen = Integer.MIN_VALUE;
+
+       while(high < s.length()){
+           
+           hash[s.charAt(high) - 'A']++;
+
+          
+
+           while((high - low + 1) - max_frequency(hash) > k){
+                hash[s.charAt(low) - 'A']--;
+                low++;
+           }
+
+           maxLen = Math.max(maxLen , high - low + 1);
+           high++;
+       }
+       if(maxLen==Integer.MIN_VALUE) return 0;
+       return maxLen;
     }
 }
