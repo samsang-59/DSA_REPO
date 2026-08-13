cb4c7e3d8e9ed7b9e3bb511aee2b407cf3206a9a
Sangram Ganta <b523059@iiit-bh.ac.in>
2026-08-13 13:48:08 +0530

    Time: 1 ms (97.53%), Space: 47.3 MB (78.64%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,24 +1,21 @@
 class Solution {
-    
+    public int find_sum(int[] nums){
+        int SUM = 0;
+        for(int i=0;i<nums.length;i++){
+            SUM += nums[i];
+        }
+        return SUM;
+    }
     public int pivotIndex(int[] nums) {
-        int[] prefix = new int[nums.length];
-        int[] suffix = new int[nums.length];
-
-        prefix[0] = 0;
-        suffix[nums.length-1] = 0; 
-        
-        // calculating the prefix part
-        for(int i=1;i<prefix.length;i++){
-            prefix[i] = prefix[i-1] + nums[i-1];
-        }
-
-        // calculating the suffix part
-        for(int i=nums.length-2;i>=0;i--){
-            suffix[i] = suffix[i+1] + nums[i+1];
-        }
+        int sum = find_sum(nums);
+        int left = 0 , right = 0;
 
         for(int i=0;i<nums.length;i++){
-            if(prefix[i]==suffix[i]) return i;
+            right = sum-left-nums[i];
+
+            if(left == right) return i;
+            
+            left += nums[i];
         }
 
         return -1;
